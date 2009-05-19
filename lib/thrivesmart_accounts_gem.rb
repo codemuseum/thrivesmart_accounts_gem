2345fb0b532b31679ba70bce975c3b3e61bc9d43
Camilo Aguilar <camilo.aguilar@gmail.com>
2009-05-18 23:58:04 -0500

    minor changes

--- lib/thrivesmart_accounts_gem.rb
+++ lib/thrivesmart_accounts_gem.rb
@@ -3,17 +3,17 @@ require 'rest_client'
 
 module ThrivesmartAccounts
   module Agent
-    THRIVESMART_ACCOUNTS_URL = "https://accounts.thrivesmart.com"
+    THRIVESMART_ACCOUNTS_URL = "http://localhost:3000"
     
     def login_from_token
       token = cookies[:auth_token]
       unless token
-        redirect_to THRIVESMART_ACCOUNTS_URL + "/login"
+        redirect_to "#{THRIVESMART_ACCOUNTS_URL}/login"
       end
       begin 
-        RestClient.post THRIVESMART_ACCOUNTS_URL + '/validate-token/' + token
+        RestClient.get "#{THRIVESMART_ACCOUNTS_URL}/validate-token/#{token}"
       rescue
-        redirect_to THRIVESMART_ACCOUNTS_URL + "/login"
+        redirect_to "#{THRIVESMART_ACCOUNTS_URL}/login"
       end
     end
   end
